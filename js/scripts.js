64a25ef23765a57eb5af90b65ded5a7a7d5874bd
girdacio <girdacio@gmail.com>
2020-03-25 00:07:37 -0300

    import versão phaser no header se necessário; colocando imagens na pasta assets; download do phaser.min.js (3.22.0); implementação do movimento para trás da nave

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,73 @@
 (function(){
 
 	var config = {
-		type: Phaser.AUTO,
-		width: 800,
-		height: 600,
-		parent: 'phaser-example',
-		scene: {
-			preload: preload,
-			create: create,
-			physics: {
-				arcade: {
-					debug: true,
-					gravity: { y: 200 }
-				},
-				matter: {
-					debug: true,
-					gravity: { y: 0.5 }
-				},
-				impact: {
-					gravity: 50,
-					debug: false,
-					setBounds: {
-						x: 150,
-						y: 50,
-						width: 500,
-						height: 500,
-						thickness: 32
-					},
-					maxVelocity: 900
-				}
-			}
-		}
-	};
+        type: Phaser.CANVAS,
+        width: 800,
+        height: 600,
+        physics: {
+	        default: 'arcade',
+	        arcade: { debug: true }
+	    },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
 
+    var game = new Phaser.Game(config);
+    var sprite;
+    var cursors;
+    var textRotacao;
+    var textAngulo;
 
-	var game = new Phaser.Game(config);
+    function preload() {    	
+        this.load.image('sky', 'assets/img/checker.png');
+        this.load.image('ship', 'assets/img/ship.png');
+    }
 
-	function preload(){
-		this.load.image('platform','img/checker.png');
-		this.load.image('ship','img/ship.png');
+    function create() {
 
-	}
+    	this.add.image(400, 300, 'sky');
 
-	function create(){
-		this.add.image(400,300,'platform');
-		this.impact.add.image(400, 300, 'ship').setActiveCollision().setVelocity(50, 50).setBounce(1);
+        //  Our player ship
+	    sprite = this.physics.add.sprite(300, 300, 'ship');
+	    sprite.setCollideWorldBounds(true);
+	 	sprite.setDamping(true);
+	    sprite.setDrag(0.99);
+	    sprite.setMaxVelocity(200);
 
-	}
+	    //  Game input
+	    cursors = this.input.keyboard.createCursorKeys();	
 
-	function update(){
+	    textRotacao = this.add.text(10, 10, '', { font: '16px Courier', fill: '#00ff00' });    
+	    textAngulo = this.add.text(10, 30, '', { font: '16px Courier', fill: '#00ff00' }); 
+    }
 
-	}
+    function update() {
+    	if (cursors.up.isDown) {
+	        this.physics.velocityFromRotation(sprite.rotation, 200, sprite.body.acceleration);
+	    }
+	    else if (cursors.down.isDown) {
+
+	        this.physics.velocityFromRotation(sprite.rotation, -200, sprite.body.acceleration);
+	    }
+	    else {
+	    	sprite.setAcceleration(0);	
+	    }
+
+	    if (cursors.left.isDown) {
+	        sprite.setAngularVelocity(-300);
+	    }
+	    else if (cursors.right.isDown) {
+	        sprite.setAngularVelocity(300);
+	    }
+	    else {
+	        sprite.setAngularVelocity(0);
+	    }
+
+	    textRotacao.setText('Rotation: ' + sprite.rotation);
+	    textAngulo.setText('Angle: ' + sprite.angle);
+    }
 
 }());
