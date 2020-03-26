3eaa08929cea67440a92cb04fd800e44e4977a52
Alcenir Costa <alcenir.1994@gmail.com>
2020-03-26 01:34:51 -0300

    Simples correção de uma linha excluida por falta de atenção e um pouco de sono

--- js/scripts.js
+++ js/scripts.js
@@ -28,16 +28,16 @@
     var game = new Phaser.Game(config);
     var container = new Phaser.Geom.Rectangle(150, 50, 500, 500,);
     function preload () {
-     this.load.image('platform','assets/img/checker.png');
-     this.load.image('ship', 'assets/img/ship.png');
-     this.load.image('box','assets/img/crate.png');
-     this.load.image('asteroid','assets/img/asteroid.png');
- }
+       this.load.image('platform','assets/img/checker.png');
+       this.load.image('ship', 'assets/img/ship.png');
+       this.load.image('box','assets/img/crate.png');
+       this.load.image('asteroid','assets/img/asteroid.png');
+   }
 
- function create () {
-  this.add.image(400,300,'platform');
-  sprite = this.physics.add.image(400, 300, 'ship');
-  sprite.setCollideWorldBounds(true);
+   function create () {
+      this.add.image(400,300,'platform');
+      sprite = this.physics.add.image(400, 300, 'ship');
+      sprite.setCollideWorldBounds(true);
         // A linha abaixo posicioina a barreira para a nave e os asteroides;
         sprite.body.setBoundsRectangle(container);
         sprite.setDamping(true);
@@ -46,11 +46,11 @@
         sprite.setMaxVelocity(200);
 
 	// Essa linha de codigo define o tamanho e posicao da 'barreira' para as caixas
-   var bounds = new Phaser.Geom.Rectangle(300, 200, 300, 300);
+ var bounds = new Phaser.Geom.Rectangle(300, 200, 300, 300);
 
-   box_group = this.physics.add.group({ immovable: true });
+ box_group = this.physics.add.group({ immovable: true });
 
-   for (var i = 0; i < 3; i++) {
+ for (var i = 0; i < 3; i++) {
             // Este laco cria e posiciona as caixas de forma aleatoria
             var pos = bounds.getRandomPoint();
             box_group.create(pos.x, pos.y, 'box');
@@ -78,38 +78,37 @@
 
         // Funcao que posiciona os asteroids de forma aleatoria dentro do container
         Phaser.Actions.RandomRectangle(asteroid.getChildren(), container);
-        this.physics.add.collider(asteroid,box_group);
+        this.physics.add.collider(asteroid,box_group,);
         this.physics.add.collider(asteroid, sprite);
-
 
         
 
     }
 
     function update () {
-       if (cursors.up.isDown) {
-           this.physics.velocityFromRotation(sprite.rotation, 200, sprite.body.acceleration);
-       }
-       else if (cursors.down.isDown) {
-           this.physics.velocityFromRotation(sprite.rotation, -200, sprite.body.acceleration);
-       }
-       else {
-           sprite.setAcceleration(0);
-       }
+     if (cursors.up.isDown) {
+         this.physics.velocityFromRotation(sprite.rotation, 200, sprite.body.acceleration);
+     }
+     else if (cursors.down.isDown) {
+         this.physics.velocityFromRotation(sprite.rotation, -200, sprite.body.acceleration);
+     }
+     else {
+         sprite.setAcceleration(0);
+     }
 
-       if (cursors.left.isDown) {
-           sprite.setAngularVelocity(-300);
-       }
-       else if (cursors.right.isDown) {
-           sprite.setAngularVelocity(300);
-       }
-       else {
-           sprite.setAngularVelocity(0);
-       }
+     if (cursors.left.isDown) {
+         sprite.setAngularVelocity(-300);
+     }
+     else if (cursors.right.isDown) {
+         sprite.setAngularVelocity(300);
+     }
+     else {
+         sprite.setAngularVelocity(0);
+     }
 
-       this.physics.world.wrap(sprite, 32);
+     this.physics.world.wrap(sprite, 32);
 
-       textRotacao.setText('Rotation: ' + sprite.rotation);
-       textAngulo.setText('Angle: ' + sprite.angle);
-   }
+     textRotacao.setText('Rotation: ' + sprite.rotation);
+     textAngulo.setText('Angle: ' + sprite.angle);
+ }
 }());
