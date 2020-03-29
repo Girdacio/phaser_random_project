439983afed6828ff2ca2d1842d4095b53df88cb2
girdacio <girdacio@gmail.com>
2020-03-29 19:42:41 -0300

    #8 - fundo ok

--- src/game.ts
+++ src/game.ts
@@ -9,38 +9,28 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.image('background','assets/img/deep-space.jpg');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+        // fundo
+        this.add.image(400,300,'background');
     }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
+    type: Phaser.CANVAS,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            fps: 60,
+            debug:true,
+            gravity: { y: 0 }
+        }
+    },
     scene: Demo
 };
 
