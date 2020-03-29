c42468994581a91a2bf97f40a5f924c2956253e2
girdacio <girdacio@gmail.com>
2020-03-29 20:28:30 -0300

    #8 - movendo nave

--- src/game.ts
+++ src/game.ts
@@ -3,6 +3,7 @@ import 'phaser';
 export default class Demo extends Phaser.Scene
 {
     private nave;
+    private teclado: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor ()
     {
@@ -23,14 +24,38 @@ export default class Demo extends Phaser.Scene
         // nave
         this.nave = this.physics.add.image(400, 300, 'ship');
         this.nave.setCollideWorldBounds(true);
-        this.nave.body.setBoundsRectangle(new Phaser.Geom.Rectangle(150, 50, 500, 500,));
+        this.nave.body.setBoundsRectangle(new Phaser.Geom.Rectangle(150, 50, 500, 500)); // TODO ajustar para pegar o retangulo no background!
         this.nave.setDamping(true);
         this.nave.setDrag(0.99);
         this.nave.setMaxVelocity(200);
+
+        // input - teclado
+        this.teclado = this.input.keyboard.createCursorKeys();
     }
 
     update ()
     {
+        // movimento da nave
+        if (this.teclado.up.isDown) {
+            this.physics.velocityFromRotation(this.nave.rotation, 200, this.nave.body.acceleration);
+        }
+        else if (this.teclado.down.isDown) {
+            this.physics.velocityFromRotation(this.nave.rotation, -200, this.nave.body.acceleration);
+        }
+        else {
+            this.nave.setAcceleration(0);
+        }
+
+        if (this.teclado.left.isDown) {
+            this.nave.setAngularVelocity(-300);
+        }
+        else if (this.teclado.right.isDown) {
+            this.nave.setAngularVelocity(300);
+        }
+        else {
+            this.nave.setAngularVelocity(0);
+        }
+
         this.physics.world.wrap(this.nave, 32); // TODO pra que serve?
     }
 }
