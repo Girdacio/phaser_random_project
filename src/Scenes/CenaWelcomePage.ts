1113396ce9fcc5e2ce795bb9f010b65967122577
girdacio <girdacio@gmail.com>
2020-04-03 23:34:02 -0300

    #16 - mundando da cena welcome para cena principal

--- src/Scenes/CenaWelcomePage.ts
+++ src/Scenes/CenaWelcomePage.ts
@@ -6,6 +6,7 @@ export default class CenaWelcomePage extends Phaser.Scene {
     game: Phaser.Game;
     music: Phaser.Sound.BaseSound;
     btnPermitirSom: Phaser.GameObjects.Sprite;
+    btnIniciar: Phaser.GameObjects.Sprite;
 
     constructor() {
         super('welcome');
@@ -13,17 +14,24 @@ export default class CenaWelcomePage extends Phaser.Scene {
 
     preload() {
         this.load.image('welcome', 'assets/img/welcome.png');
+        this.load.image('iniciar', 'assets/img/btn_iniciar.png');
         this.load.image('permissao', 'assets/img/btn_permitir_audio.png');
         this.load.audio('welcomeAudio', 'assets/audio/oedipus_ark_pandora.mp3');
     }
 
     create() {        
         this.titleScreenImage = this.add.sprite(400, 300, 'welcome');
+
+        this.btnIniciar = this.add.sprite(400, 300, 'iniciar').setInteractive();
+        this.btnIniciar.once('pointerdown', function () {
+            this.music.stop();  // stop na música dessa cena
+            this.scene.start('principal'); // chamar próxima cena
+        }, this);
         
         this.btnPermitirSom = this.add.sprite(400, 300, 'permissao').setInteractive();
-        this.btnPermitirSom.on('pointerdown', function () {
+        this.btnPermitirSom.once('pointerdown', function () {
             this.destroy(); // esconder botão de permissão
-        });
+        });      
         
         this.music = this.sound.add('welcomeAudio');          
     }
