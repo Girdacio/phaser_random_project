e060a37f65a036d2d0cc4f41a9bad60b15b01a81
girdacio <girdacio@gmail.com>
2020-04-05 17:31:16 -0300

    ajustando posição das caixas

--- src/GameObjects/Caixas.ts
+++ src/GameObjects/Caixas.ts
@@ -1,7 +1,5 @@
 export class Caixas extends Phaser.Physics.Arcade.StaticGroup {
-    private varsX;
-    private varsY;
-    private pos;
+    
     constructor(scene: Phaser.Scene, boxes_container: Phaser.Geom.Rectangle, bounds: Phaser.Geom.Rectangle) {
 
         super(scene.physics.world, scene);
@@ -11,38 +9,41 @@ export class Caixas extends Phaser.Physics.Arcade.StaticGroup {
             frameQuantity: 3,
             classType: Phaser.Physics.Arcade.Image
         });
-        let i = 0;
-        this.varsX = [];
-        this.varsY = [];
 
         this.getChildren().forEach((caixa: Phaser.Physics.Arcade.Image) => {
-        this.pos = Phaser.Geom.Rectangle.RandomOutside(boxes_container, bounds);
-
-            if (i == 0) {
-                caixa.body.reset(Math.ceil(this.pos.x), Math.ceil(this.pos.y));
-                this.varsX.push(caixa.x);
-                this.varsY.push(caixa.y);
-                console.info(caixa.x);
-            } else {
-                this.pos = Phaser.Geom.Rectangle.RandomOutside(boxes_container, bounds);
-                for (let ii = 0; i < this.varsX.length; i++) {
-                    if (this.pos.x < this.varsX[ii] + 50 && this.pos.x > this.varsY[ii] - 50 && this.pos.y < this.varsY[ii] + 50 && this.pos.y > this.varsY[ii] - 50) {
-                        this.pos = Phaser.Geom.Rectangle.RandomOutside(boxes_container, bounds);
-                        ii = 0;
-                    }
-                }
-                caixa.body.reset(Math.ceil(this.pos.x), Math.ceil(this.pos.y));
-                this.varsX.push(caixa.x);
-                this.varsY.push(caixa.y);
-                console.info(caixa.x);
-                console.info(this.varsX.length)
-
-            }
-            // this.pos = Phaser.Geom.Rectangle.RandomOutside(boxes_container, bounds);
-            // console.info(this.pos.x, this.pos.y);
-            // caixa.body.reset(this.pos.x, this.pos.y);
-            i++;
+            let posicao;
+            // loop para gerar a posição de cada caixa até que ela não intersecta com as outras
+            do {
+                posicao = Phaser.Geom.Rectangle.RandomOutside(boxes_container, bounds);
+                caixa.body.reset(posicao.x, posicao.y);
+            } while (this.conflito(posicao, caixa));
         });
+    }
 
+    conflito(posicao: Phaser.Geom.Rectangle, caixaTeste) {
+
+        let caixaAtual;
+        let intersects;
+
+        for(let i = 0; i < this.getLength() - 1; i++) {
+
+            caixaAtual = this.getChildren()[i];
+
+            if (caixaAtual == caixaTeste) {
+                continue;
+            }
+
+            let bodyCaixaAtual: any = caixaAtual.body;
+            let boundsCaixaAtual = new Phaser.Geom.Rectangle(bodyCaixaAtual.x, bodyCaixaAtual.y, bodyCaixaAtual.width, bodyCaixaAtual.height);
+
+            intersects = Phaser.Geom.Intersects.RectangleToRectangle(boundsCaixaAtual, posicao);            
+
+            if (intersects) {
+                alert('ajustando posição das caixas!');
+                return true;
+            }
+        }
+
+        return false;
     }
 }
